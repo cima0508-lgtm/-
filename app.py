e1a38a6b0c80d586dfecbb75802d5734f09d35ad
cima0508-lgtm <cima0508@gmail.com>
2026-03-03 09:58:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,13 +15,10 @@ st.set_page_config(
 st.markdown("""
     <style>
         .block-container {
-            padding-top: 4rem; /* 1remから4remに増やして、さらに下に下げます */
+            padding-top: 2rem; /* 4remから2remに減らして、間隔を詰める */
         }
     </style>
 """, unsafe_allow_html=True)
-
-# タイトル表示
-st.markdown("<h1 style='text-align: center;'>芦北の「たんぼ」</h1>", unsafe_allow_html=True)
 
 # ブラウザの自動翻訳を防ぐための設定
 st.markdown('<meta name="google" content="notranslate">', unsafe_allow_html=True)
